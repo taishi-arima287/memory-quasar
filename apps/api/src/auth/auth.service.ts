4e8324ded52ecd810b6b4a6574ceacac782c3b68
taishi-arima287 <taishi@arima-system.com>
2025-01-13 17:36:52 +0900

    fix api userテーブルのカラム名を変更

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
     }
 
     const payload = { sub: user.id, email: user.email };
-    
+
     return {
       access_token: this.jwtService.sign(payload),
       user: {
