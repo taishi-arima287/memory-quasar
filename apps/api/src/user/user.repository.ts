56c806e8b4d2a10d4a75d64f83d387d72aa21356
taishi-arima287 <taishi@arima-system.com>
2025-01-13 23:49:42 +0900

    add ユーザ情報取得

---
+++ apps/api/src/user/user.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { GetUserRequest, User } from "./dto/user.dto";
+
+@Injectable()
+export class UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(getUserRequest: GetUserRequest): Promise<User | null> {
+    try {
+      return await this.prisma.user.findUnique({
+        where: { id: getUserRequest.id },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException("ユーザー情報の取得に失敗しました");
+    }
+  }
+}
