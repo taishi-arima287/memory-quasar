2167282eef7b33223f1356813e2c528ccfaf345a
taishi-arima287 <taishi@arima-system.com>
2025-01-17 20:55:47 +0900

    fix web & admin next.configを統合

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
   transpilePackages: ["@memory-quasar/shared"],
   experimental: {
     externalDir: true,
