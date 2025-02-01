d4046656a11f077103a0acd5818a3a8dfa102ad1
taishi-arima287 <taishi@arima-system.com>
2025-02-02 01:19:57 +0900

    fix ドキュメントテーブルにタグを追加

--- apps/api/prisma/migrations/20250201154627_init/migration.sql
+++ apps/api/prisma/migrations/20250201154627_init/migration.sql
@@ -3,11 +3,11 @@ CREATE TYPE "DocumentVisibility" AS ENUM ('PRIVATE', 'PUBLIC', 'SPACE');
 
 -- CreateTable
 CREATE TABLE "users" (
-    "id" CHAR(36) NOT NULL,
+    "id" VARCHAR(36) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "space_id" CHAR(36),
+    "space_id" VARCHAR(36),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -16,7 +16,7 @@ CREATE TABLE "users" (
 
 -- CreateTable
 CREATE TABLE "spaces" (
-    "id" CHAR(36) NOT NULL,
+    "id" VARCHAR(36) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "owner_id" VARCHAR(36) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -27,9 +27,9 @@ CREATE TABLE "spaces" (
 
 -- CreateTable
 CREATE TABLE "documents" (
-    "id" CHAR(36) NOT NULL,
-    "user_id" CHAR(36) NOT NULL,
-    "space_id" CHAR(36),
+    "id" VARCHAR(36) NOT NULL,
+    "user_id" VARCHAR(36) NOT NULL,
+    "space_id" VARCHAR(36),
     "title" VARCHAR(100) NOT NULL,
     "content" TEXT NOT NULL,
     "visibility" "DocumentVisibility" NOT NULL DEFAULT 'PRIVATE',
@@ -41,11 +41,33 @@ CREATE TABLE "documents" (
     CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "tags" (
+    "id" VARCHAR(36) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_DocumentTags" (
+    "A" VARCHAR(36) NOT NULL,
+    "B" VARCHAR(36) NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
 CREATE INDEX "documents_space_id_visibility_idx" ON "documents"("space_id", "visibility");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_DocumentTags_AB_unique" ON "_DocumentTags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_DocumentTags_B_index" ON "_DocumentTags"("B");
 
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_space_id_fkey" FOREIGN KEY ("space_id") REFERENCES "spaces"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -55,3 +77,9 @@ ALTER TABLE "documents" ADD CONSTRAINT "documents_space_id_fkey" FOREIGN KEY ("s
 
 -- AddForeignKey
 ALTER TABLE "documents" ADD CONSTRAINT "documents_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_DocumentTags" ADD CONSTRAINT "_DocumentTags_A_fkey" FOREIGN KEY ("A") REFERENCES "documents"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_DocumentTags" ADD CONSTRAINT "_DocumentTags_B_fkey" FOREIGN KEY ("B") REFERENCES "tags"("id") ON DELETE CASCADE ON UPDATE CASCADE;
